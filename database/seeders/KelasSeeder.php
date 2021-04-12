03d21e564c6e17414368247af26a40710007b036
auzanzulhazmi <zulhazmiauzan@gmail.com>
2021-04-12 18:18:57 +0700

    Buat kelas seeder

--- database/seeders/KelasSeeder.php
+++ database/seeders/KelasSeeder.php
@@ -13,6 +13,18 @@ class KelasSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $kelas = [
+            ['nama_kelas' => 'TI 2A'],
+            ['nama_kelas' => 'TI 2B'],
+            ['nama_kelas' => 'TI 2C'],
+            ['nama_kelas' => 'TI 2D'],
+            ['nama_kelas' => 'TI 2E'],
+            ['nama_kelas' => 'TI 2F'],
+            ['nama_kelas' => 'TI 2G'],
+            ['nama_kelas' => 'TI 2H'],
+        ];
+
+        DB::table('kelas')->insert($kelas);
+
     }
 }
